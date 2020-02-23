46285328a0c07b4953819f33277803be42bfc34b
liza.metla <liza.metla@gmail.com>
2020-02-23 19:49:02 +0300

    base added

---
+++ newone/newone/ProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileViewController.swift
+//  newone
+//
+//  Created by Елизавета Метла on 2/21/20.
+//  Copyright © 2020 Елизавета Метла. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var backToWelcome: UINavigationBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ newone/newone/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  newone
+//
+//  Created by Елизавета Метла on 2/21/20.
+//  Copyright © 2020 Елизавета Метла. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var showPasswordButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+       }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
